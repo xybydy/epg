5f507dfdb6bf50d672f82b2883eb7d51a6a322b7
Fatih Ka <xybydy@gmail.com>
2020-11-04 23:12:40 +0300

    qwe

--- golib/mongo/models.go
+++ golib/mongo/models.go
@@ -6,5 +6,4 @@ type ChannelMatch struct {
 	ChanName string `bson:"chan_name" json:"chan_name"`
 	TvgID    string `bson:"tvg_id" json:"tvg_id"`
 	UserName string `bson:"user_name" json:"user_name"`
-
 }
